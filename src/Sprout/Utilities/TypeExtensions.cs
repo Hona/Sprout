12f23435e73f09a1ee08ad37d5c2c3cbbd4adb37
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-11-28 09:26:40 +1000

    🐛 Fix nullability

--- src/Sprout/Utilities/TypeExtensions.cs
+++ src/Sprout/Utilities/TypeExtensions.cs
@@ -5,13 +5,13 @@ namespace Sprout.Utilities;
 
 public static class TypeExtensions
 {
-    public static List<Type?> GetBlockGenerators(this Assembly assembly) 
+    public static List<Type?> GetBlockGenerators(this Assembly assembly)
     {
         var it = typeof (IBlockGenerator<>);
 
         return GetClassesImplementingAnInterface(assembly, it);
     }
-    
+
     public static List<Type?> GetClassesImplementingAnInterface(Assembly? assemblyToScan, Type? implementedInterface)
     {
         if (assemblyToScan == null)
@@ -35,7 +35,7 @@ public static class TypeExtensions
             typesInTheAssembly = e.Types.Where(t => t != null);
         }
 
-        var classesImplementingInterface = new List<Type>();
+        var classesImplementingInterface = new List<Type?>();
 
         // if the interface is a generic interface
         if (implementedInterface.IsGenericType)
@@ -43,12 +43,12 @@ public static class TypeExtensions
             foreach (var typeInTheAssembly in typesInTheAssembly)
             {
                 if (!typeInTheAssembly.IsClass) continue;
-                
+
                 var typeInterfaces = typeInTheAssembly.GetInterfaces();
                 foreach (var typeInterface in typeInterfaces)
                 {
                     if (!typeInterface.IsGenericType) continue;
-                    
+
                     var typeGenericInterface = typeInterface.GetGenericTypeDefinition();
                     var implementedGenericInterface = implementedInterface.GetGenericTypeDefinition();
 
@@ -64,7 +64,7 @@ public static class TypeExtensions
             foreach (var typeInTheAssembly in typesInTheAssembly)
             {
                 if (!typeInTheAssembly.IsClass) continue;
-                
+
                 // if the interface is a non-generic interface
                 if (implementedInterface.IsAssignableFrom(typeInTheAssembly))
                 {
